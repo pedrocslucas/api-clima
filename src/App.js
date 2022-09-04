82ae4d951a718cea5915e304d4005e492cbfdbc6
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2022-09-04 11:54:04 -0300

    finalizando o projeto inicial com APIs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 function App() {
 
   const [city, setCity] = useState("")
+  const [weatherForecast, setWeatherForecast] = useState(null)
 
   const handleChange = (event) => {
     setCity(event.target.value)
@@ -10,19 +11,29 @@ function App() {
 
   const handleSearch = () => {
     fetch(`http://api.weatherapi.com/v1/current.json?key=7a757c4b8a4e43afb1f111827220409&q=${city}&lang=pt`)
+    .then((response) => {
+      if(response.status === 200){
+        return response.json()
+      }else{
+        return null
+      }
+    })
+    .then((data) => {
+      setWeatherForecast(data)
+    });
   }
 
   return (
     <div>
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <a className="navbar-brand text-white" href="#stop">
+      <header className="bg-dark mb-4 p-3">
+        <h2 className="text-white text-center" href="#stop">
           Api de Clima
-        </a>
-      </nav>
+        </h2>
+      </header>
       <main className="container">
         <div className="jumbotron">
           <h1>
-            Verifique agora a previsão do tempo da sua cidade!
+            Verifique agora a previsão do tempo na sua cidade!
           </h1>
           <p className="lead">
             Digite o nome da sua cidade no campo abaixo e em seguida clique em pesquisar.
@@ -30,17 +41,46 @@ function App() {
 
           <div className="row mb-4">
             <div className="col-md-6">
-              <input 
-              onChange={handleChange} 
-              className="form-control" 
-              value={city}/>
+              <input
+                onChange={handleChange}
+                className="form-control"
+                value={city} />
             </div>
           </div>
 
-          <button className="btn btn-primary btn-lg">
+          <button onClick={handleSearch} className="btn btn-primary btn-lg" id="btnSearch">
             Pesquisar
           </button>
-        
+
+          {
+            weatherForecast ? (
+              <div className="mt-4 d-flex">
+                <div>
+                  <img src={weatherForecast.current.condition.icon}/>
+                </div>
+
+                <div className="text-dark">
+                  <h3>Hoje o dia está {weatherForecast.current.condition.text} em {weatherForecast.location.name} no {weatherForecast.location.country}</h3>
+                  <p>
+                    Temperatura: {weatherForecast.current.temp_c}°C
+                  </p>
+                  <p>
+                    Sensação Térmica: {weatherForecast.current.feelslike_c}°C
+                  </p>
+                  <p>
+                    Umidade do Ar: {weatherForecast.current.humidity}%
+                  </p>
+                </div>
+
+              </div>
+            ) : 
+              <div className="mt-4">
+                <h3 className="text-danger">
+                  Não consegui encontrar sua cidade =(
+                </h3>
+              </div>
+          }
+
         </div>
       </main>
 
