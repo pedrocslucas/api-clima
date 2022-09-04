114f6a6694eb21cbf0303d7326e051bc91fc36b9
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2022-09-04 09:53:00 -0300

    create app in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white" href="#stop">
+          Api de Clima
         </a>
-      </header>
+      </nav>
+      <main className="container">
+        <div className="jumbotron">
+          <h1>
+            Verifique agora a previsão do tempo da sua cidade!
+          </h1>
+          <p className="lead">
+            Digite o nome da sua cidade no campo abaixo e em seguida clique em pesquisar.
+          </p>
+
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input className="form-control"/>
+            </div>
+          </div>
+
+          <button className="btn btn-primary btn-lg">
+            Pesquisar
+          </button>
+        
+        </div>
+      </main>
+
+      <footer className="footer bg-dark">
+        <p className="text-white p-3 text-center">Desenvolvido com ReactJS e Bootstrap por <span className="text-warning">Pedro Lucas</span>.</p>
+      </footer>
     </div>
   );
 }
